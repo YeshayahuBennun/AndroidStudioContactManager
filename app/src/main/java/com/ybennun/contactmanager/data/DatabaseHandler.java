b9b607e137e51848296c6c63e4e9355df6415766
YeshayahuBennun <ybennun@yahoo.com>
2022-02-06 20:55:41 +0200

    commit#3 - Contact Manager App - Database Handler Class Setup

--- app/src/main/java/com/ybennun/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/ybennun/contactmanager/data/DatabaseHandler.java
@@ -6,24 +6,37 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.ybennun.contactmanager.R;
+import com.ybennun.contactmanager.util.Util;
+
 import java.sql.SQLData;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(Context context,
-                           String name,
-                           SQLiteDatabase.CursorFactory factory,
-                           int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
+    //We create our table
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //SQL - Structured Query Language
+        /*
+            create table_name(id, name, phone_number);
+        */
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + " TEXT,"
+                + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
 
+        sqLiteDatabase.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        String DROP_TABLE = String.valueOf(R.string.db_drop);
+        sqLiteDatabase.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
 
+        //Create a table again
+        onCreate(sqLiteDatabase);
     }
 }
