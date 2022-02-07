91c7d26b76793f85c9957367a6c611883a2ea39c
YeshayahuBennun <ybennun@yahoo.com>
2022-02-07 18:49:19 +0200

    commit#4 - Contact Manager App - CRUD - Add and Get Contact Methods

--- app/src/main/java/com/ybennun/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/ybennun/contactmanager/data/DatabaseHandler.java
@@ -1,12 +1,15 @@
 package com.ybennun.contactmanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
 import com.ybennun.contactmanager.R;
+import com.ybennun.contactmanager.model.Contact;
 import com.ybennun.contactmanager.util.Util;
 
 import java.sql.SQLData;
@@ -38,5 +41,49 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         //Create a table again
         onCreate(sqLiteDatabase);
+
+    }
+
+    /*
+        CRUD = Create, Read, Update, Delete
+    */
+
+    //Add Contact
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME, contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        //Insert to row
+        db.insert(Util.TABLE_NAME, null, values);
+        db.close();//Closing db connection!
+    }
+
+    //Get a Contact.
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME,
+                new String[]{Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER},
+                Util.KEY_ID + "=?", new String[]{String.valueOf(id)},
+                null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact();
+
+        contact.setId(Integer.parseInt(cursor.getString(0)));
+        contact.setName(cursor.getString(1));
+        contact.setPhoneNumber(cursor.getString(2));
+
+        /*  Cursor position
+            0     1       2
+           id   name     phone
+         */
+
+        return contact;
     }
 }
